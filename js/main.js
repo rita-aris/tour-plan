ecc701c31e2cb1174f47998ada46079d9877ac76
rita-aris <ritaaris@yandex.ru>
2021-05-10 22:23:32 +0400

    create: added keyboard in slider

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ const hotelSwiper = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: ".hotel-slider__button--next",
