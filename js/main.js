8e7f1d597cb5b93d5e223aed5a5eda05b200bd7a
rita-aris <ritaaris@yandex.ru>
2021-05-13 01:47:24 +0400

    create: started to make reviews block

--- js/main.js
+++ js/main.js
@@ -13,3 +13,19 @@ const hotelSwiper = new Swiper(".hotel-slider", {
     prevEl: ".hotel-slider__button--prev",
   },
 });
+
+const reviewsSwiper = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+});
