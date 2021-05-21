bdaa60dacd50df1ea3aeab8296c84e57ee3df8f1
rita-aris <ritaaris@yandex.ru>
2021-05-21 21:41:18 +0400

    fix: added overflow for modal window

--- js/main.js
+++ js/main.js
@@ -46,12 +46,16 @@ $(document).ready(function () {
     var modalDialog = $(".modal__dialog");
     modalOverlay.addClass("modal__overlay--visible");
     modalDialog.addClass("modal__dialog--visible");
+    $("body").css("overflow", "hidden");
   }
   function closeModal(event) {
+    $("body").css("overflow", "auto");
+    if (event.key === "Escape" || event.type === "click") {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
     event.preventDefault();
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
   }
 });
